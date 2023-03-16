8ad28a7ae608ddb63331edd5c7435dc4ee0ad229
nidroy <nidroy@mail.ru>
2023-03-17 00:36:17 +0300

    add material

--- AddMaterial.cs
+++ AddMaterial.cs
@@ -15,6 +15,24 @@ namespace Contract
         public AddMaterial()
         {
             InitializeComponent();
+            SelectMaterial();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            SelectMaterial();
+        }
+
+        private void SelectMaterial()
+        {
+            DataSet ds = DatabaseContext.ExecuteQuery(
+                "SELECT " +
+                "[name] as [Название], " +
+                "[unit] as [Единица измерения], " +
+                "[price] as [Цена] " +
+                "FROM [Material];");
+
+            dataGridView.DataSource = ds.Tables[0].DefaultView;
         }
     }
 }
